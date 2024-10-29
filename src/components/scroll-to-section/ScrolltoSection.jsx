6d34373f71e8f5c8d9c8a7f56c87f4db44dd2592
OneClickTechy <oneclicktechy@gmail.com>
2024-10-30 02:08:50 +0530

    saved

--- src/components/scroll-to-section/ScrolltoSection.jsx
+++ src/components/scroll-to-section/ScrolltoSection.jsx
@@ -17,19 +17,22 @@ const ScrolltoSection = () => {
     },
   ];
   const handleScrolltoSection = (e) => {
-    
     const pos = currScrollRef.current.getBoundingClientRect().top;
     window.scrollTo({
-        top: pos,
-        behavior: "smooth"
-    })
-  }
+      top: pos,
+      behavior: "smooth",
+    });
+  };
   return (
     <div>
       <h1 className="text-4xl">Scroll to Section</h1>
       <button onClick={handleScrolltoSection}>Click to Scroll</button>
       {cards.map((card, index) => (
-        <div key={index} ref={index === 2 ? currScrollRef : null} className={card.styles}>
+        <div
+          key={index}
+          ref={index === 2 ? currScrollRef : null}
+          className={card.styles}
+        >
           {card.title}
         </div>
       ))}
